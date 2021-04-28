71fc6ada4396cfa13983ee90b3b52afd10341b90
Aaron <jenkins.770@osu.edu>
2021-04-28 10:49:54 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib}/>
+          <Route exact path="/ohterpage" component={OtherPage}/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
